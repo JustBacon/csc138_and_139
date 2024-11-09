78180b75f89c1ba2546c1dfe5cff7bcb24abf80c
JustBacon <christianbuco17@gmail.com>
2024-11-08 16:19:03 -0800

    MEMALLOC FINISHED!

--- csc139/memalloc/main.c
+++ csc139/memalloc/main.c
@@ -1,33 +1,73 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
 #include "umem.h"
 
+// list of test
 void test_allocation();
 void test_deallocation();
 void test_reallocation();
-void test_allocation_strategies();
+void test_bestfit();
+void test_worstfit();
+void test_firstfit();
+void test_nextfit();
+
+// used to initialize 64KB
+void initialize_memory(int allocation_algorithm);
 
 int main() {
-    if (umeminit(64 * 1024, BEST_FIT) != 0) {
-        fprintf(stderr, "Error: Failed to initialize memory allocator\n");
-        return 1;
+    // array of tests
+    void (*tests[])() = {
+        test_allocation,
+        test_deallocation,
+        test_reallocation,
+        test_bestfit,
+        test_worstfit,
+        test_firstfit,
+        test_nextfit
+    };
+
+    int num_tests = sizeof(tests) / sizeof(tests[0]);
+    pid_t pid;
+
+    // using different process to do testing
+    for (int i = 0; i < num_tests; i++) {
+        if ((pid = fork()) == 0) {
+            tests[i]();
+            exit(0);
+        } else if (pid > 0) {
+            wait(NULL);
+        } else {
+            // Fork failed
+            fprintf(stderr, "Error: Fork failed\n");
+            return 1;
+        }
     }
-    umemstats();
-    printf("\n");
-
-    // test_allocation();
-    // test_deallocation();
-    test_reallocation();
-
+    
     return 0;
 }
 
+// initialize memory allocator
+void initialize_memory(int allocation_algorithm) {
+    // 64KB
+    if (umeminit(64 * 1024, allocation_algorithm) != 0) {
+        fprintf(stderr, "Error: Failed to initialize memory allocator\n");
+        exit(1);
+    }
+}
+
+// test umalloc function
 void test_allocation() {
     printf("=== TEST ALLOCATION ===\n");
+    initialize_memory(BEST_FIT);
 
     // Actually will allocate 104 bytes because 8-byte aligned chunks of memory
     void *ptr1 = umalloc(100);
     if(ptr1) {
         printf("Allocated 100 bytes at %p\n", ptr1);
+
     } else {
         printf("Failed allocation of 100 bytes\n");
     }
@@ -46,12 +86,15 @@ void test_allocation() {
         printf("Failed allocation of 20000 bytes\n");
     }
 
-    // Memory allocated should be 20304 bytes (does not include the sizeof(header_t))
+    // Memory allocated should be 104 + 200 + 20000 = 20304 bytes (does not include the sizeof(header_t))
     umemstats();
+    printf("\n");
 }
 
+// test ufree function
 void test_deallocation() {
     printf("=== TEST DEALLOCATION ===\n");
+    initialize_memory(BEST_FIT);
 
     void *ptr1 = umalloc(16);
     void *ptr2 = umalloc(32);
@@ -63,26 +106,148 @@ void test_deallocation() {
         ufree(ptr2);
         printf("Freed memory at %p\n", ptr2);
         umemstats();
+        printf("\n");
     }
 }
 
+// test urealloc function
 void test_reallocation() {
     printf("=== TEST REALLOCATION ===\n");
+    initialize_memory(BEST_FIT);
 
     void *ptr1 = umalloc(100);
+    printf("Allocated 100 bytes at %p\n", ptr1);
 
-    if (ptr1) {
-        printf("Allocated 100 bytes at %p\n", ptr1);
-
-        void *ptr2 = urealloc(ptr1, 200);
-        if (ptr2) {
-            printf("Reallocated to 200 bytes at %p\n", ptr2);
-        }
-
-        void *ptr3 = urealloc(ptr2, 50);
-        if (ptr3) {
-            printf("Reallocated to 50 bytes at %p\n", ptr3);
-        }
-    }
+    // will umalloc(200) since size is bigger
+    void *ptr2 = urealloc(ptr1, 200);
+    printf("Reallocated to 200 bytes at %p\n", ptr2);
+    
+    void *ptr3 = urealloc(ptr2, 50);
+    printf("Reallocated to 50 bytes at %p\n", ptr3);
+    
+    printf("ptr1(freed): %p\nptr2(allocated somewhere else): %p\nptr3(same address as ptr2): %p\n", ptr1, ptr2, ptr3);
+    
     umemstats();
+    printf("\n");
+}
+
+// algorithm test for best fit
+void test_bestfit() {
+    printf("=== TEST BEST FIT ALGORITHM ===\n");
+    initialize_memory(BEST_FIT);
+
+    void *ptr1 = umalloc(1000);
+    printf("(1)Allocated 1000 bytes at %p\n", ptr1);
+    void *ptr2 = umalloc(2000);
+    printf("(2)Allocated 2000 bytes at %p\n", ptr2);
+    void *ptr3 = umalloc(500);
+    printf("(3)Allocated 500 bytes at %p\n", ptr3);
+    void *ptr4 = umalloc(1500);
+    printf("(4)Allocated 1500 bytes at %p\n", ptr4);
+
+    ufree(ptr2);
+    printf("Freed memory at %p\n", ptr2);
+    ufree(ptr4);
+    printf("Freed memory at %p\n", ptr4);
+
+    // best fit algorithm should choose the 1500-byte for allocating 1200-byte
+    void *ptr5 = umalloc(1200);
+    printf("Allocated 1200 bytes at %p\n", ptr5);
+    printf("BEST FIT should choose (4) 1500-byte for allocating 1200-byte\n");
+
+    umemstats();
+    printf("\n");
+}
+
+// algorithm test for worst fit
+void test_worstfit() {
+    printf("=== TEST WORST FIT ALGORITHM ===\n");
+    initialize_memory(WORST_FIT);
+
+    void *ptr1 = umalloc(1000);
+    printf("(1)Allocated 1000 bytes at %p\n", ptr1);
+    void *ptr2 = umalloc(3000);
+    printf("(2)Allocated 3000 bytes at %p\n", ptr2);
+    void *ptr3 = umalloc(500);
+    printf("(3)Allocated 500 bytes at %p\n", ptr3);
+    void *ptr4 = umalloc(1500);
+    printf("(4)Allocated 1500 bytes at %p\n", ptr4);
+
+    ufree(ptr2);
+    printf("Freed memory at %p\n", ptr2);
+    ufree(ptr4);
+    printf("Freed memory at %p\n", ptr4);
+
+    // worst fit algorithm should choose the 3000-byte for allocating 1200-byte
+    void *ptr5 = umalloc(1200);
+    printf("Allocated 1200 bytes at %p\n", ptr5);
+    printf("WORST FIT should choose the biggest block 60000-byte that has yet to be allocated for allocating 1200-byte.(it will create a block)\n");
+
+    umemstats();
+    printf("\n");
+}
+
+// algorithm test for first fit
+void test_firstfit() {
+    printf("=== TEST FIRST FIT ALGORITHM ===\n");
+    initialize_memory(FIRST_FIT);
+
+    void *ptr1 = umalloc(1000);
+    printf("(1)Allocated 1000 bytes at %p\n", ptr1);
+    void *ptr2 = umalloc(3000);
+    printf("(2)Allocated 3000 bytes at %p\n", ptr2);
+    void *ptr3 = umalloc(500);
+    printf("(3)Allocated 500 bytes at %p\n", ptr3);
+    void *ptr4 = umalloc(1500);
+    printf("(4)Allocated 1500 bytes at %p\n", ptr4);
+
+    ufree(ptr2);
+    printf("Freed memory at %p\n", ptr2);
+    ufree(ptr4);
+    printf("Freed memory at %p\n", ptr4);
+
+    // first fit algorithm should choose the 3000-byte for allocating 1200-byte
+    void *ptr5 = umalloc(1200);
+    printf("Allocated 1200 bytes at %p\n", ptr5);
+    printf("FIRST FIT should choose the first available block. (4) was freed last,\nso it is the first node in the free list. 1500-byte for allocating 1200-byte\n");
+
+    umemstats();
+    printf("\n");
+}
+
+// algorithm test for next fit
+void test_nextfit() {
+    printf("=== TEST NEXT FIT ALGORITHM ===\n");
+    initialize_memory(NEXT_FIT);
+
+    void *ptr1 = umalloc(1000);
+    printf("(1)Allocated 1000 bytes at %p\n", ptr1);
+    void *ptr2 = umalloc(2000);
+    printf("(2)Allocated 2000 bytes at %p\n", ptr2);
+    void *ptr3 = umalloc(1500);
+    printf("(3)Allocated 1500 bytes at %p\n", ptr3);
+    void *ptr4 = umalloc(500);
+    printf("(4)Allocated 500 bytes at %p\n", ptr4);
+    void *ptr5 = umalloc(3000);
+    printf("(5)Allocated 3000 bytes at %p\n", ptr5);
+    void *ptr6 = umalloc(3000);
+    printf("(6)Allocated 3000 bytes at %p\n", ptr6);
+
+    ufree(ptr2);
+    printf("Freed memory at %p\n", ptr2);
+    ufree(ptr4);
+    printf("Freed memory at %p\n", ptr4);
+    ufree(ptr6);
+    printf("Freed memory at %p\n", ptr6);
+    // free list right now is ptr6(3000) -> ptr4(500) -> ptr2(2000) -> remaining free
+
+    void *ptr7 = umalloc(1200);
+    printf("Allocated 1200 bytes at %p\n", ptr7);
+    printf("NEXT FIT should choose (6) 3000-byte for allocating 1200-byte\n");
+    void *ptr8 = umalloc(1200);
+    printf("Allocated 1200 bytes at %p\n", ptr8);
+    printf("NEXT FIT should skip (4) and choose (2) 2000-byte for allocating 1200-byte\n");
+
+    umemstats();
+    printf("\n");
 }
--- csc139/memalloc/umem.c
+++ csc139/memalloc/umem.c
@@ -3,39 +3,60 @@
 #include <unistd.h>
 #include <sys/mman.h>
 #include "umem.h"
+#include <string.h>
 
-static node_t *free_list = NULL;
-static header_t *allocated_region = NULL;
-static int allocation_algorithm;
-static size_t total_allocated = 0;
-static int total_allocations = 0;
-static int total_deallocations = 0;
-static node_t *next_fit_pointer = NULL;
+static node_t *free_list = NULL;        // first node of the free list
+static int allocation_algorithm;        // determine which algorithm. BEST_FIT, WORST_FIT,etc
+static size_t total_allocated = 0;      // used for umemstats. how many bytes allocated
+static int total_allocations = 0;       // keep track of umalloc() usage
+static int total_deallocations = 0;     // keep track of ufree() usage
+static node_t *next_fit_pointer = NULL; // used for NEXT_FIT strategy
 
+// find_block searches for a free block to allocate
+node_t *find_block(size_t size, size_t totalSize);
+
+// used for splitting block
+node_t *find_best_previous_block(node_t *best);
+
+// algorithm functions
+node_t *find_best_fit_block(size_t size);
+node_t *find_worst_fit_block(size_t size);
+node_t *find_first_fit_block(size_t totalSize);
+node_t *find_next_fit_block(size_t totalSize);
+
+// Initializes memory allocator
+// sizeOfRegion is the number of bytes to request from OS using mmap()
+// allocationAlgo determines which algorithm to use
 int umeminit(size_t sizeOfRegion, int allocationAlgo) {
+    // check if user uses 0 or negative size
     if (sizeOfRegion <= 0) {
         fprintf(stderr, "Error: Invalid memory size\n");
         return -1;
     }
 
+    // check if umeminit is called more than once in a process
     if (free_list != NULL) {
         fprintf(stderr, "Error: Memory Allocater already exist\n");
         return -1;
     }
 
+    // round up the requested memory in units of page size
     int pageSize = getpagesize();
     sizeOfRegion = (sizeOfRegion + pageSize - 1) & ~(pageSize - 1);
-    printf("%li\n", sizeOfRegion);
 
+    // use mmap() to request memory from OS
     void *region = mmap(NULL, sizeOfRegion, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
 
+    // check if mmap() failed
     if (region == MAP_FAILED) {
         fprintf(stderr, "Error: mmap failed");
         return -1;
     }
 
+    // set allocation algorithm
     allocation_algorithm = allocationAlgo;
     
+    // set free list pointer
     free_list = region;
     free_list->size = sizeOfRegion - sizeof(header_t);
     free_list->next = NULL;
@@ -43,86 +64,24 @@ int umeminit(size_t sizeOfRegion, int allocationAlgo) {
     return 0;
 }
 
+// takes size in bytes to be allocated and returns a pointer
 void *umalloc(size_t size) {
+    // if free_list does not exist return NULL
     if (!free_list) return NULL;
 
+    // round up the requested memory in units of 8
     size = (size + (8 - 1)) & ~(8 - 1);
     size_t totalSize = size + sizeof(header_t);
 
-    node_t *prev = NULL;
-    node_t *best = NULL;
-    node_t *best_prev = NULL;
-    node_t *curr = free_list;
-
-    switch (allocation_algorithm) {
-        case BEST_FIT:
-            while (curr) {
-                if (curr->size >= size && (!best || curr->size < best->size)) {
-                    best = curr;
-                    best_prev = prev;
-                }
-                prev = curr;
-                curr = curr->next;
-            }
-            break;
-        
-        case WORST_FIT:
-            while (curr) {
-                if (curr->size >= size && (!best || curr->size > best->size)) {
-                    best = curr;
-                    best_prev = prev;
-                }
-                prev = curr;
-                curr = curr->next;
-            }
-            break;
-
-        case FIRST_FIT:
-            while (curr) {
-                if (curr->size >= totalSize) {
-                    best = curr;
-                    best_prev = prev;
-                }
-                prev = curr;
-                curr = curr->next;
-            }
-            break;
-        
-        case NEXT_FIT:
-            if(next_fit_pointer == NULL) {
-                next_fit_pointer = free_list;
-            }
-            curr = next_fit_pointer;
-            while (curr) {
-                if (curr->size >= totalSize) {
-                    best = curr;
-                    best_prev = prev;
-                    next_fit_pointer = curr->next;
-                    break;
-                }
-                prev = curr;
-                curr = curr->next;
-            }
-            if (!best) {
-                curr = free_list;
-                prev = NULL;
-                while (curr != next_fit_pointer) {
-                    if (curr->size >= totalSize) {
-                        best = curr;
-                        best_prev = prev;
-                        next_fit_pointer = curr->next;
-                        break;
-                    }
-                    prev = curr;
-                    curr = curr->next;
-                }
-            }
-            break;
-    }
+    // find best block to use to allocate memory based on algorithm
+    node_t *best = find_block(size, totalSize);
 
     if (!best) {
         return NULL;
     }
+
+    // use for splitting block
+    node_t *best_prev = find_best_previous_block(best);
 
     // Splitting block
     if (best->size >= totalSize + sizeof(node_t)) {
@@ -153,6 +112,108 @@ void *umalloc(size_t size) {
     return (void *)((char *)header + sizeof(header_t));
 }
 
+// use for splitting block
+node_t *find_best_previous_block(node_t *best) {
+    node_t *prev = NULL;
+    node_t *curr = free_list;
+
+    while (curr && curr != best) {
+        prev = curr;
+        curr = curr->next;
+    }
+
+    return prev;
+}
+
+// find best block to use based on allocation algorithm
+node_t *find_block(size_t size, size_t totalSize) {
+    switch (allocation_algorithm) {
+        case BEST_FIT:
+            return find_best_fit_block(size);
+        case WORST_FIT:
+            return find_worst_fit_block(size);
+        case FIRST_FIT:
+            return find_first_fit_block(totalSize);
+        case NEXT_FIT:
+            return find_next_fit_block(totalSize);
+    }
+    return NULL;
+}
+
+// algorithm for BEST FIT
+node_t *find_best_fit_block(size_t size) {
+    node_t *best = NULL;
+    node_t *curr = free_list;
+
+    while (curr) {
+        if (curr->size >= size && (!best || curr->size < best->size)) {
+            best = curr;
+        }
+        curr = curr->next;
+    }
+    return best;
+}
+
+// algorithm for WORST FIT
+node_t *find_worst_fit_block(size_t size) {
+    node_t *best = NULL;
+    node_t *curr = free_list;
+
+    while (curr) {
+        if (curr->size >= size && (!best || curr->size > best->size)) {
+            best = curr;
+        }
+        curr = curr->next;
+    }
+    return best;
+}
+
+// algorithm for FIRST FIT
+node_t *find_first_fit_block(size_t totalSize) {
+    node_t *curr = free_list;
+
+    while (curr) {
+        if (curr->size >= totalSize) {
+            return curr;
+        }
+        curr = curr->next;
+    }
+    return NULL;
+}
+
+// algorithm for NEXT FIT
+node_t *find_next_fit_block(size_t totalSize) {
+    if (next_fit_pointer == NULL) {
+        next_fit_pointer = free_list;
+    }
+    node_t *prev = NULL;
+    node_t *curr = next_fit_pointer;
+
+    // Search from the current pointer onwards
+    while (curr) {
+        if (curr->size >= totalSize) {
+            next_fit_pointer = curr->next;
+            return curr;
+        }
+        prev = curr;
+        curr = curr->next;
+    }
+
+    // If no suitable block is found, wrap around and search from the beginning
+    curr = free_list;
+    while (curr != next_fit_pointer) {
+        if (curr->size >= totalSize) {
+            next_fit_pointer = curr->next;
+            return curr;
+        }
+        prev = curr;
+        curr = curr->next;
+    }
+
+    return NULL;
+}
+
+// frees memory object that ptr points to
 int ufree(void *ptr) {
     if (!ptr) return 0;
 
@@ -171,7 +232,6 @@ int ufree(void *ptr) {
         curr = curr->next;
     }
 
-    printf("%li\n", header->size);
     total_deallocations++;
     total_allocated -= header->size;
 
@@ -180,6 +240,8 @@ int ufree(void *ptr) {
     new_free->next = free_list;
     free_list = new_free;
 
+    // check neighbouring blocks to see if they are also free
+    // Coalesce adjacent free blocks
     curr = free_list;
     while (curr != NULL && curr->next != NULL) {
         node_t *next = curr->next;
@@ -222,15 +284,20 @@ void *urealloc(void *ptr, size_t size) {
     }
 
     // if the block size is sufficient, return the same pointer
-    if (header->size >= size) return ptr;
+    if (header->size >= size) {
+        return ptr;
+    }
 
     void *new_ptr = umalloc(size);
     if (!new_ptr) return NULL;
+
+    memcpy(new_ptr, ptr, header->size);
 
     ufree(ptr);
     return new_ptr;
 }
 
+// show stats
 void umemstats(void) {
     size_t free_memory = 0;
     size_t largest_free_block = 0;
@@ -253,7 +320,7 @@ void umemstats(void) {
         current = free_list;
         while (current != NULL) {
             if (current->size < fragmentation_threshold) {
-                small_free_memory += current->size;
+                small_free_memory += current->size + sizeof(header_t);
             }
             current = current->next;
         }
